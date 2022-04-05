1a5ab3588df3732feb84c69f7e5242ca3d8494db
Ubuntu <gopaddle@gopaddle-test.lgc2ftladuqehhp2wfjfee1ofd.ix.internal.cloudapp.net>
2022-04-05 11:45:48 +0000

    updated mongodb url

--- startScript.sh
+++ startScript.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd dist
-export MONGODB_URI=mongodb+srv://hphpe:hdTlWN9GXSCN2Zry@cluster0.oc2cf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority 
+export MONGODB_URI='mongodb+srv://hphpe:hdTlWN9GXSCN2Zry@cluster0.oc2cf.mongodb.net/mern_social?retryWrites=true&w=majority'
+echo $MONGODB_URI
 npm start
