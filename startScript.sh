5b8ef6ec3ed88c0c59f41b7f1c2dd8d09726eb8c
Ubuntu <gopaddle@gopaddle-test.lgc2ftladuqehhp2wfjfee1ofd.ix.internal.cloudapp.net>
2022-04-05 07:22:00 +0000

    removed old line

--- startScript.sh
+++ startScript.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd dist
-#MONGODB_URI=mongodb://34.81.202.212:30000 npm start
 MONGODB_URI=mongodb+srv://hphpe:hdTlWN9GXSCN2Zry@cluster0.oc2cf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority npm start
