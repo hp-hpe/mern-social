e16f38af472b08e3760900c30d1b20243f4f7d2e
Ubuntu <gopaddle@gopaddle-test.lgc2ftladuqehhp2wfjfee1ofd.ix.internal.cloudapp.net>
2022-04-06 10:36:40 +0000

    set port

--- startScript.sh
+++ startScript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd dist
 export MONGODB_URI='mongodb+srv://hphpe:hdTlWN9GXSCN2Zry@cluster0.oc2cf.mongodb.net/mern_social?retryWrites=true&w=majority'
+export PORT=3030
 echo $MONGODB_URI
+echo $PORT
 npm start
