e9ceac98637d68e7ac903579f78752561d82874e
Ubuntu <gopaddle@gopaddle-test.lgc2ftladuqehhp2wfjfee1ofd.ix.internal.cloudapp.net>
2022-04-07 04:29:03 +0000

    Fix env var issue

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+process.env.MONGODB_URI='mongodb+srv://hphpe:hdTlWN9GXSCN2Zry@cluster0.oc2cf.mongodb.net/mern_social?retryWrites=true&w=majority'
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
